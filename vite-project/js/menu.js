a0cb83199564762b06f935375beae69d4ade3b40
blairweng7 <144455825+blairweng7@users.noreply.github.com>
2023-11-16 00:22:10 -0500

    jnaieofan

--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -1,10 +1,103 @@
 const Menu = [
     {
-        Name:
-        Type
-
+      name: "Classic Burger",
+      description: "A juicy beef patty with lettuce, tomato, and pickles on a sesame seed bun.",
+      price: 9.99,
+      vegetarian: false,
+      glutenFree: false,
+      spicy: false
+    },
+    {
+      name: "Vegetarian Pizza",
+      description: "A pizza topped with fresh vegetables, tomato sauce, and mozzarella cheese.",
+      price: 12.99,
+      vegetarian: true,
+      glutenFree: false,
+      spicy: false
+    },
+    {
+      name: "Grilled Chicken Salad",
+      description: "Tender grilled chicken breast on a bed of mixed greens with cherry tomatoes and balsamic vinaigrette.",
+      price: 8.99,
+      vegetarian: false,
+      glutenFree: true,
+      spicy: false
+    },
+    {
+      name: "Spicy Tofu Stir-Fry",
+      description: "Stir-fried tofu with a mix of vegetables in a spicy soy-based sauce.",
+      price: 10.99,
+      vegetarian: true,
+      glutenFree: true,
+      spicy: true
+    },
+    {
+      name: "Seafood Pasta",
+      description: "Pasta with a medley of seafood, garlic, and a creamy Alfredo sauce.",
+      price: 14.99,
+      vegetarian: false,
+      glutenFree: false,
+      spicy: false
+    },
+    {
+      name: "Margherita Pizza",
+      description: "A classic pizza with tomato, fresh mozzarella, and basil leaves.",
+      price: 11.99,
+      vegetarian: true,
+      glutenFree: false,
+      spicy: false
+    },
+    {
+      name: "BBQ Chicken Wings",
+      description: "Crispy chicken wings glazed with barbecue sauce, served with ranch dressing.",
+      price: 7.99,
+      vegetarian: false,
+      glutenFree: false,
+      spicy: true
+    },
+    {
+      name: "Vegetable Sushi Roll",
+      description: "A roll of sushi with a variety of fresh vegetables and rice, served with soy sauce.",
+      price: 13.99,
+      vegetarian: true,
+      glutenFree: false,
+      spicy: false
+    },
+    {
+      name: "Caesar Salad",
+      description: "Romaine lettuce, croutons, and parmesan cheese tossed in Caesar dressing.",
+      price: 6.99,
+      vegetarian: false,
+      glutenFree: false,
+      spicy: false
+    },
+    {
+      name: "Shrimp Tacos",
+      description: "Grilled shrimp in corn tortillas with cabbage slaw and chipotle mayo.",
+      price: 15.99,
+      vegetarian: false,
+      glutenFree: false,
+      spicy: true
+    },
+    {
+      name: "Penne alla Vodka",
+      description: "Penne pasta in a creamy vodka-infused tomato sauce.",
+      price: 12.99,
+      vegetarian: true,
+      glutenFree: false,
+      spicy: false
+    },
+    {
+      name: "Chocolate Brownie Sundae",
+      description: "Warm chocolate brownie topped with vanilla ice cream and chocolate syrup.",
+      price: 8.99,
+      vegetarian: true,
+      glutenFree: false,
+      spicy: false
     }
-
-    
-];
+  ];
+  
+  // Accessing individual items in the menu array
+  console.log(menu[0]); // Print details of the first menu item (Classic Burger)
+  
 export {Menu};
