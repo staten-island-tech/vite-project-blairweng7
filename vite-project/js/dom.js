25ee8dd39d9dd039fe011af6ce556dc18dccb5d2
blairweng7 <144455825+blairweng7@users.noreply.github.com>
2023-11-26 23:22:22 -0500

    sanfe;aofe

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -1,6 +1,16 @@
 const DOMSelectors = {
-    button: DOMSelectors.getElementbyID ()
-    form:DOMSelectors.querySelector()
-menu: DOMSelectors.querySelector(Menu)
-description: DOMSelectors
-}
+  form: document.querySelector("#form"),
+  container: document.getElementById("container"),
+  card: DOMSelectors.querySelector('#box'),
+  all: DOMSelectors.querySelector("#all-btn"),
+  breakfast: DOMSelectors.querySelector("#breakfast-btn"),
+  lunch: DOMSelectors.querySelector("#lunch-btn"),
+  dinner: DOMSelectors.querySelector("#dinner-btn"),
+  vegetarian: DOMSelectors.querySelector("#vegetarian-btn"),
+  under10: DOMSelectors.querySelector("#under10-btn"),
+  over10: DOMSelectors.querySelector("#over10-btn"),
+  drink: DOMSelectors.querySelector("#drink-btn"),
+  dessert: DOMSelectors.querySelector("#dessert-btn"),
+};
+
+export { DOMSelectors };
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,10 +1,29 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from '../counter.js'
+import './style.css';
+import { Menu } from './menu';
+import { DOMSelectors } from './dom'
+
+const types = {
+  breakfast: Menu.filter((breakfast)=> breakfast.type === 'breakfast'),
+  lunch: Menu.filter((lunch)=> lunch.type === 'lunch'),
+  dinner: Menu.filter((dinner)=> dinner.type === 'dinner'),
+  dessert: Menu.filter((dessert)=> dessert.type === 'dessert'),
+  vegetarian: Menu.filter((vegetarian)=> vegetarian.vegetarian === true),
+  under10: Menu.filter((cost)=> cost.price <= 10),
+  over10: Menu.fitler((cost)=> cost.price > 10),
+}
+function addcard() {
+  DOMSelectors.container.insertAdjacentHTML(
+    'afterend'
+    `
+    div "class"= card id="box"
+    
+    `
+  )
+}
 
 
-document.querySelector('#app').innerHTML = `
+
+/*document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
@@ -21,7 +40,6 @@ document.querySelector('#app').innerHTML = `
     </p>
   </div>
 `
-
 setupCounter(document.querySelector('#counter'))
 document.querySelector(".btn").addEventListener("click", function(){
   
@@ -43,5 +61,4 @@ function addcard() {
         console.log(menu)
   );
 }
-
-
+*/
