08639b719c10abfa10707d4012e8a46a8d6b9f55
blairweng7 <144455825+blairweng7@users.noreply.github.com>
2023-11-29 09:23:27 -0500

    dgsghsrg

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -3,8 +3,6 @@ const DOMSelectors = {
   breakfast: document.getElementById("breakfast"),
   lunch: document.getElementById("lunch"),
   dinner: document.getElementById("dinner"),
-  vegetarian: document.getElementById("vegetarian"),
-  dessert: document.getElementById("dessert"),
   form: document.getElementById("filter-btn"),
   cards: document.querySelector(".cards"),
   foodName: document.querySelector(".food-name"),
